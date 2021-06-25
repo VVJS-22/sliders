48576e84f1c41940d391753025c3802d821e01f9
VVJS-22 <s.jayeshchennai@gmail.com>
2021-06-25 08:36:42 +0530

    second commit

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
 
-const slides = ["b1","b2","b3"]
+const slides = ["b1","b2","b3"];
 
-function show(button) {
+
+function show(button,move) {
     console.log(button.id);
-    hidePrevious();
-    const slide = document.querySelector(`.${button.id}`);
-    const visible = slide.classList.remove("opacity");
-    console.log(slide);
+    // hidePrevious();
+    // const slide = document.querySelector(`.${button.id}`);
+    // const visible = slide.classList.remove("opacity");
+    // console.log(slide);
+    const slideDeck = document.querySelector('.slide-deck');
+    slideDeck.style.transform = `translateX(${move}%)`
 }
 
 function hidePrevious() {
